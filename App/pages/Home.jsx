3b3a00770fea0da17746ba431222ec8267cc0ac2
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2023-11-12 21:48:20 -0800

    added bots image

--- App/pages/Home.jsx
+++ App/pages/Home.jsx
@@ -1,10 +1,89 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import ChatFaceData from '../services/chatFaceData';
+import { useState } from 'react';
 
 export default function Home() {
+  const [chatFaceData, setChatFaceData] = useState([]);
+  const [selectedChatFace, setSelectedChatFace] = useState();
+
+  useEffect(() => {
+    setChatFaceData(ChatFaceData);
+    setSelectedChatFace(ChatFaceData[0]);
+  }, []);
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View
+      style={{
+        alignItems: 'center',
+        paddingTop: 90,
+      }}
+    >
+      <Text
+        // [] is used to give the multiple inline styles
+        style={[
+          {
+            color: selectedChatFace?.primary,
+          },
+          {
+            fontSize: 30,
+          },
+        ]}
+      >
+        Hello
+      </Text>
+      <Text
+        style={[
+          {
+            color: selectedChatFace?.primary,
+          },
+          {
+            fontSize: 30,
+            fontFamily: 'sans-serif',
+            fontWeight: 'bold',
+          },
+        ]}
+      >
+        I am {selectedChatFace?.name}
+      </Text>
+      <Image
+        source={{
+          uri: selectedChatFace?.image,
+        }}
+        style={{
+          width: 200,
+          height: 200,
+          marginTop: 30,
+        }}
+      />
+      <Text
+        style={{
+          fontSize: 25,
+          marginTop: 30,
+        }}
+      >
+        How can i help you?{' '}
+      </Text>
+      {/* flatlist */}
+      <FlatList
+        data={chatFaceData}
+        horizontal={true}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              margin: 13,
+            }}
+          >
+            <Image
+              source={{ uri: item.image }}
+              style={{
+                width: 50,
+                height: 50,
+              }}
+            />
+          </View>
+        )}
+      />
     </View>
   );
 }
