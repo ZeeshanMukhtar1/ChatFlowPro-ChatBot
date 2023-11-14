a5f522bf625687ac5c59f82ea95c75df8d808e91
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2023-11-14 07:03:18 -0800

    💚 fixed paths bug

--- App/services/globalapi.js
+++ App/services/globalapi.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// Update this with your Vercel deployment URL
 const BASE_URL = 'https://nextjsbanckend.vercel.app/api/bardapi';
 
 const getBardApi = (userMsg) => axios.get(`${BASE_URL}?ques=${userMsg}`);
